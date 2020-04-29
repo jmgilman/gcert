3e237420e64c2bb06f8bcb4017b7693c8da48403
Joshua Gilman <joshuagilman@gmail.com>
2020-04-29 16:33:16 -0700

    Adds initial proto implementation for the certificate service

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	cservice "github.com/jmgilman/gcert/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type certServer struct {
+
+}
+
+func (c *certServer) GetCertificate(ctx context.Context, in *cservice.CertificateRequest) (*cservice.CertificateResponse, error) {
+	return &cservice.CertificateResponse{}, nil
+}
+
+func main() {
+	fmt.Println("Hello grpc!")
+
+	flag.Parse()
+	lis, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	cservice.RegisterCertificateServiceServer(grpcServer, &certServer{})
+	grpcServer.Serve(lis)
+}
